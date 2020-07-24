75aa825e5560857a2c0c9975a145c22939eeff10
Adolf-asdf <1778527665@qq.com>
2020-07-24 18:11:48 +0800

    标签

--- src/main/java/learn/example/community/controller/PublishController.java
+++ src/main/java/learn/example/community/controller/PublishController.java
@@ -1,10 +1,12 @@
 package learn.example.community.controller;
 
 
+import learn.example.community.cache.TagCache;
 import learn.example.community.dto.QuestionDTO;
 import learn.example.community.model.Question;
 import learn.example.community.model.User;
 import learn.example.community.service.QuestionService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,49 +26,58 @@ public class PublishController {
 
     @GetMapping("/publish/{id}")
     public String edit(@PathVariable(name = "id") Long id,
-                       Model model){
+                       Model model) {
         QuestionDTO question = questionService.getById(id);
-        model.addAttribute("title",question.getTitle());
-        model.addAttribute("description",question.getDescription());
-        model.addAttribute("tag",question.getTag());
-        model.addAttribute("id",question.getId());
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id", question.getId());
+
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish(Model model) {
+        model.addAttribute("tags", TagCache.get());
         return "publish";
     }
 
     @PostMapping("/publish")
-    public String doPulish(
-            @RequestParam(value="title",required = false) String title,
-            @RequestParam(value="description",required = false) String description,
-            @RequestParam(value="tag",required = false) String tag,
-            @RequestParam(value="id",required = false) Long id,
+    public String doPublish(
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Long id,
             HttpServletRequest request,
-            Model model){
+            Model model) {
 
-        model.addAttribute("title",title);
-        model.addAttribute("description",description);
-        model.addAttribute("tag",tag);
+        model.addAttribute("title", title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
+        model.addAttribute("tags", TagCache.get());
 
-        if (title==null||title==""){
-            model.addAttribute("error","标题不能为空");
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
             return "publish";
         }
-        if (description==null||description==""){
-            model.addAttribute("error","问题补充不能为空");
+        if (description == null || description == "") {
+            model.addAttribute("error", "问题补充不能为空");
             return "publish";
         }
-        if (tag==null||tag==""){
-            model.addAttribute("error","标签不能为空");
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+        String invalid = TagCache.filterInvalid(tag);
+        if (StringUtils.isNoneBlank(invalid)) {
+            model.addAttribute("error", "输入非法标签" + invalid);
             return "publish";
         }
 
-        User user=(User) request.getSession().getAttribute("user");
-        if (user==null){
-            model.addAttribute("error","用户未登陆");
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            model.addAttribute("error", "用户未登陆");
             return "publish";
         }
 
